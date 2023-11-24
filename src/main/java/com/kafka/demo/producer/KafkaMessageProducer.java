389daf073794e3f8c7e63da29b01f33a28a66ff1
simone-art <simoneperiodista3@gmail.com>
2023-11-24 14:25:28 -0300

    Configuração do properties e Producer

---
+++ src/main/java/com/kafka/demo/producer/KafkaMessageProducer.java
@@ -0,0 +1,39 @@
+package com.kafka.demo.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@Slf4j
+public class KafkaMessageProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value(value = "${message.topic.name:profesorp}")
+    private String topicName;
+
+    public void sendMessage(String topic, String message) {
+        if (topic==null || topic.trim().equals(""))
+            topic=topicName;
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        future.whenComplete((result, ex) ->  {
+            if (ex == null) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        message + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+
+}
